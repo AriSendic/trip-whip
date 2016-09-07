971852b0425e7be7f114edab180184a791aaa38d
AriSendic <rabbisendic@gmail.com>
2016-09-06 21:28:18 -0500

    model for dropdown buttons

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
-  # get '/projects' => 'projects#index'
+  get '/projects' => 'projects#index'
   # get 'projects/:id' => 'projects#show'
   # get '/projects/:id/edit' => 'projects#edit'
 
