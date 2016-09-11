1003e2a4a08bacf5ee00d867bf7d748d7914772c
AriSendic <rabbisendic@gmail.com>
2016-09-11 00:08:31 -0500

    added logo and date to pdf

--- app/models/itinerary_pdf.rb
+++ app/models/itinerary_pdf.rb
@@ -1,12 +1,15 @@
 class ItineraryPdf < Prawn::Document
-  def initialize(itinerary)
+  def initialize(itinerary, view)
     super()
     @itinerary = itinerary
+    @view = view
+    logo 
     itinerary_name
     venues
     restaurants
+   
   end
-  
+
   def itinerary_name
     text "Your trip: \##{@itinerary.title}", size: 30, style: :bold
   end
@@ -24,14 +27,27 @@ class ItineraryPdf < Prawn::Document
   end
 
   def restaurants
-    move_down 20
-    table restaurant_rows
+    if @itinerary.restaurants.count > 0 
+      move_down 20
+      table restaurant_rows
+    end
   end  
    
   def restaurant_rows
-    [["Restaurant Name", "Address", "Phone"]] +
-      @itinerary.restaurants.map do |restaurant| 
-        [restaurant.name, restaurant.address, restaurant.phone]
-      end
+    if @itinerary.restaurants.count > 0
+      [["Restaurant Name", "Address", "Phone"]] +
+        @itinerary.restaurants.map do |restaurant| 
+          [restaurant.name, restaurant.address, restaurant.phone]
+        end
+    end  
   end
+  
+  def logo
+    logopath = "#{Rails.root}/app/assets/images/logo.png"
+    image logopath, :width => 197, :height => 79
+    text "Created on #{@itinerary.created_at.strftime('%A, %b %d')}"
+    move_down 20 
+  end 
+
+   
 end
