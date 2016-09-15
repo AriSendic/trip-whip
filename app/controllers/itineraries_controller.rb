30183ee9ddb671cbd70af759fbdb5e156c1e728c
AriSendic <rabbisendic@gmail.com>
2016-09-14 21:31:15 -0500

    styling

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -23,9 +23,9 @@ class ItinerariesController < ApplicationController
   def show
     @itinerary = Itinerary.find_by(id: params[:id])
     @times = [0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6]
-    parts = @itinerary.venues.first.address.split(' ')
-    @city = parts[-3]
-    @state = parts[-2]
+    @state = @itinerary.venues.first.address.split(' ')[-2]
+    @city = @itinerary.venues.first.address.split(', ')[-2]
+    Unirest.timeout(5)
     @data = Unirest.get("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22#{@city}%2C%20#{@state}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys").body
     respond_to do |format|
       format.html
