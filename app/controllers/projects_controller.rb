39a059aa893953c7c699238fd6a7f00198436f71
AriSendic <arimsendic@gmail.com>
2016-11-15 15:13:20 -0600

    removed test material from Projects folders

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,12 +2,14 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.all
   end
+
   def show
   
   end
+
   def edit
+
+  end  
   
-  #   @data = Unirest.get("https://maps.googleapis.com/maps/api/place/textsearch/json?query=zoos+near+Chicago&key=#{ENV['GOOGLE_PLACES_KEY']}").body
-  # render json: @data
-  end
+  
 end
